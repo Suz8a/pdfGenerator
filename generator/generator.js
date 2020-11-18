6a0ec83442c6f5e9b567fe7fa98247b8bc7ac9d3
Adriana Ochoa <15171173@itculiacan.edu.mx>
2020-11-18 14:02:19 -0700

    template added

--- generator/generator.js
+++ generator/generator.js
@@ -3,13 +3,14 @@ const puppeteer = require("puppeteer");
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+  // html path or url
   await page.goto(
     "file:///Users/suz8a/Desktop/pdf-generator/template/template.html",
     {
       waitUntil: "networkidle2",
     }
   );
-  await page.pdf({ path: "hn.pdf", format: "A4" });
+  await page.pdf({ path: "hn.pdf", format: "A4", printBackground: true });
 
   await browser.close();
 })();
